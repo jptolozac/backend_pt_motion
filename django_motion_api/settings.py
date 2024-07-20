bb3d9f66a53945cc2cceea5766b01257acacb1ba
jptolozac <jeanpaultoloza@gmail.com>
2024-07-20 13:19:40 -0500

    Add: allowing all CORS permissions

--- django_motion_api/settings.py
+++ django_motion_api/settings.py
@@ -47,9 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_seed',
-    'corsheaders',
     'rest_framework',
     'coreapi',
+    'corsheaders',
     'dealer',
 ]
 
@@ -147,7 +147,39 @@ if not DEBUG:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # cors authorization
-CORS_ALLOWED_ORIGINS = []
+
+# CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    'https://frontend-pt-motion.vercel.app'
+]
+
+# CORS_ORIGIN_WHITELIST = [
+#     "http://localhost:5173",
+#     'https://frontend-pt-motion.vercel.app'
+# ]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema"
